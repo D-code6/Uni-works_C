a894be4de9e6495d31598e948736d38eec51fad9
D-code6 <81482109+D-code6@users.noreply.github.com>
2021-03-27 21:36:18 +0300

    Screen outout (5-5-6)
    1-integer tanımladık
    2-integer değer verdik
    3-bir arttırıp yazdırdık

---
+++ c++ örnek (5-5-6 ).c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+	int c;
+
+	c = 5;
+	printf("%d\n", c);
+	printf("%d\n", c++);
+	printf("%d\n\n", c);
+
+	c = 5;
+	printf("%d\n", c);
+	printf("%d\n", c++);
+	printf("%d\n\n", c);
+
+	return 0;
+
+
+
+
+
+}
